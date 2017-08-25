b2ab6f81390a84837d78c9638554605e514c9ab8
Mark Swillus <ms42Q@users.noreply.github.com>
2017-08-25 11:08:48 +0200

    Conveniently upload volumes to Mega.nz

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+bash -c  "nohup mega-cmd" &
+sleep 1
+if [[ "$USERNAME" != "NOBODY" ]] && [[ "$PASSWORD" != "CHANGEME" ]]; then
+    mega-login $USERNAME $PASSWORD
+else
+    echo "Please specify a valid username and password with -e USERNAME and -e PASSWORD. Aborting."
+    exit 1
+fi
+if [ -d $TARGET ]; then
+    ARCHIVENAME=$PREFIX$(echo _$(date --iso-8601)_$(date +%s))
+    if [[ -f "/gpg/public.key" ]] && [[ "$GPG_ID" != "NONE" ]]; then
+        gpg --import --armor /gpg/private.key
+        ARCHIVENAME=$ARCHIVENAME.gpg.zip
+        gpg-zip -r $GPG_ID --gpg-args "--batch --trust-model always" -e -o $ARCHIVENAME $TARGET/*
+        TARGET=$ARCHIVENAME
+    else
+        ARCHIVENAME=$ARCHIVENAME.tar.gz
+        tar -czvf $ARCHIVENAME $TARGET/*
+    fi
+else
+    echo "Please mount the dir that you want to upload to $TARGET or set \$TARGET to an existing dir. Aborting."
+    exit 1
+fi
+mega-put $ARCHIVENAME
+exec $@
+
